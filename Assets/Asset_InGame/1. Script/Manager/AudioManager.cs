26657b132172c6addaddf38986c20b037d720204
hxetrp52 <yessung0865@gmail.com>
2026-01-12 23:15:02 +0900

    오디오 매니저 스크립트 추가

---
+++ Assets/Asset_InGame/1. Script/Manager/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AudioManager : ManagerBase
+{ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource bgmAudioSource;
+
+    public override void Init()
+    {
+        Debug.Log("사운드 시작 재생 뿌슝빠슝!!!");
+    }
+
+    public override void ManagerUpdate()
+    {
+        Debug.Log("사운드 연속 재생 뿌슝빠슝!!!");
+    }
+}
