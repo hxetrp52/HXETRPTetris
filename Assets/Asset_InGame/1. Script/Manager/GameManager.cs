830b1425748515439306ceacf269ed66a639aaf7
hxetrp52 <yessung0865@gmail.com>
2026-01-12 23:01:02 +0900

    게임매니저 스크립트 추가

---
+++ Assets/Asset_InGame/1. Script/Manager/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    private Dictionary<Type, ManagerBase> managerContainer = new Dictionary<Type, ManagerBase>();
+
+    public List<ManagerBase> managers;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        AddManagers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var manager in managerContainer.Values)
+        {
+            manager.ManagerUpdate();
+        }
+    }
+
+    private void AddManagers() // 매니저 추가
+    {
+        foreach (var manager in managers) 
+        {
+            managerContainer.Add(manager.GetType(), manager);
+            manager.Init();
+        }
+    }
+
+    public T GetManager<T>() where T : ManagerBase // 매니저 외부에서 가져오기
+    {
+        if (managerContainer.TryGetValue(typeof(T), out var comp))
+            return (T)comp;
+
+        return default;
+    }
+}
---
+++ Assets/Asset_InGame/1. Script/Manager/ManagerBase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class ManagerBase : MonoBehaviour
+{
+    public abstract void Init();
+
+    public abstract void ManagerUpdate();
+}
